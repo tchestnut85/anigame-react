42df9f2126affd83f6d115c0fd53d8c402ca2815
Tom Chestnut <tchestnut85@gmail.com>
2021-08-14 01:34:56 -0400

    Updated the Search component for getting/setting localStorage and the NavBar component to display the saved searches and enables the delete button to clear displayed searches.

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,23 @@
+import { CLEAR_STORAGE } from '../../utils/context/searchActions';
 import React from 'react';
 import { Search } from '../Search/Search';
+import { capitalizeWords } from '../../utils/helpers';
+import { clearSearches } from '../../utils/localStorage';
+import { useSearchContext } from '../../utils/context/SearchState';
+
+// TODO 1 - Add handleSearch function to search for a saved title when clicked its button
+// TODO 2 - Seperate the mapped buttons out into a separate component
 
 export const NavBar = () => {
+	const [{ savedSearches }, dispatch] = useSearchContext();
+
+	const handleClear = () => {
+		dispatch({ type: CLEAR_STORAGE });
+		clearSearches();
+	};
+
+	// const handleSearch = () => {};
+
 	return (
 		<nav
 			id='search-display'
@@ -10,11 +26,29 @@ export const NavBar = () => {
 			aria-label='main navigation'
 		>
 			<div id='gametitle-buttons' className='buttons'>
-				<button id='delete-btn' className='button is-danger is-rounded'>
+				<button
+					id='delete-btn'
+					className='button is-danger is-rounded'
+					onClick={handleClear}
+				>
 					<span className='icon is-small'>
 						<i className='fas fa-skull'></i>
 					</span>
 				</button>
+				{savedSearches.length
+					? savedSearches.map((item, i) => {
+							console.log(`item ${i}:`, item);
+							return (
+								<button
+									key={i}
+									className='button game-button is-rounded is-light'
+									// onClick={handleSearch}
+								>
+									{capitalizeWords(item)}
+								</button>
+							);
+					  })
+					: null}
 			</div>
 			<div className='navbar-end'>
 				<Search />

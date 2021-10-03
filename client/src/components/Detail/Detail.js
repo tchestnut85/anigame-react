f2fc2c52f7f8d779fce427906c84e02d0566e45e
Tom Chestnut <tchestnut85@gmail.com>
2021-10-03 12:15:57 -0400

    Finished updating the Detail component

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -7,9 +7,7 @@ import { useSearchContext } from '../../utils/context/SearchState';
 
 export const Detail = () => {
 	const [state] = useSearchContext();
-	console.log('state:', state);
 	const { details } = state;
-	console.log('details:', details);
 	const history = useHistory();
 
 	const isGame = details?.type === options.game;
@@ -51,31 +49,31 @@ export const Detail = () => {
 						<div
 							className={`${styles.details} columns column is-10`}
 						>
-							<div
-								id='detail-img'
-								className='column is-one-third'
-							>
-								<img
-									src={
-										details?.attributes?.coverImage
-											?.original
-									}
-									alt={details?.attributes?.canonicalTitle}
-								/>
-							</div>
-							<div
-								id='detail-info'
-								className={`column is-two-thirds`}
-							>
+							<div id='detail-info' className={`column`}>
 								<h3 className={styles.title}>
 									{details?.attributes?.canonicalTitle}
 								</h3>
+								<div id='detail-img'>
+									<img
+										src={
+											details?.attributes?.coverImage
+												?.original
+										}
+										alt={
+											details?.attributes?.canonicalTitle
+										}
+									/>
+								</div>
 								<div className='columns'>
-									<p className='column subtitle is-6 has-text-right'>
-										Released:{' '}
+									<p className='column subtitle is-6 is-centered has-text-centered'>
+										<b>Released:</b>{' '}
 										{getYear(
 											details?.attributes?.createdAt
 										)}
+									</p>
+									<p className='column subtitle is-6 has-text-centered'>
+										<b>Rating:</b>{' '}
+										{details?.attributes?.ageRating}
 									</p>
 								</div>
 								<div className='columns'>

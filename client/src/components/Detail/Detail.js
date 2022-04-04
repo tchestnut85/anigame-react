147fa00825d97f0279f481970d76f4cfa4ac4729
Tom Chestnut <tchestnut85@gmail.com>
2022-04-04 19:54:29 -0400

    fixed issue causing crash

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -3,16 +3,15 @@ import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 
 import { getYear } from '../../utils/helpers';
-import { options } from '../../constants/detailsOptions';
 import styles from './Detail.module.css';
 
 export const Detail = () => {
-  const detailId = useSelector(state => state?.game.detailId);
-
-  const { image, name, release_date, genres, description } = useSelector(
-    state => state.game.games.find(game => game.id === detailId)
-  );
   const history = useHistory();
+
+  const detailId = useSelector(state => state?.game.detailId);
+  const game = useSelector(state =>
+    state.game.games.find(game => game.id === detailId)
+  );
 
   if (!detailId) {
     history.push('/anigame-react');
@@ -23,20 +22,20 @@ export const Detail = () => {
       {!!detailId && (
         <div className={`${styles.details} columns column is-10`}>
           <div id="detail-img" className="column is-one-third">
-            <img src={image?.original} alt={name} />
+            <img src={game?.image?.original} alt={game?.name} />
           </div>
           <div id="detail-info" className={`column is-two-thirds`}>
-            <h3 className={styles.title}>{name}</h3>
+            <h3 className={styles.title}>{game?.name}</h3>
             <div className="columns">
               <p className="column subtitle is-6 has-text-right">
-                <b>Released:</b> {getYear(release_date)}
+                <b>Released:</b> {getYear(game?.release_date)}
               </p>
               <p className="column subtitle is-6 has-text-left">
-                <b>Genre:</b> {genres[0].name}
+                <b>Genre:</b> {game?.genres[0].name}
               </p>
             </div>
             <div className="columns">
-              <p className="column is-centered">{description}</p>
+              <p className="column is-centered">{game?.description}</p>
             </div>
           </div>
         </div>

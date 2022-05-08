73ef08492806c7e751774d80042473717df0b7ac
Tom Chestnut <tchestnut85@gmail.com>
2022-05-08 13:43:37 -0400

    added h4 mixin; converted Hero, ConsoleButtons to css modules; moved other scss styles to appropriate css modules

--- client/src/components/ConsoleButtons/ConsoleButtons.js
+++ client/src/components/ConsoleButtons/ConsoleButtons.js
@@ -2,6 +2,8 @@ import { ConsoleBtn } from './ConsoleBtn';
 import React from 'react';
 import { consoles } from '../../constants/consoleList';
 
+import styles from './ConsoleButtons.module.scss';
+
 // TODO - setup the theme color context to change the background and font colors based on the console button clicked
 
 export const ConsoleButtons = () => {
@@ -9,9 +11,7 @@ export const ConsoleButtons = () => {
     <div>
       <section className="section pb-6 mb-6">
         <div className="has-text-centered">
-          <h1 id="console-wars" className="pb-4">
-            Console Wars
-          </h1>
+          <h1 className={`${styles.heading} pb-4`}>Console Wars</h1>
           <h2 className="pb-4">Choose Your Console's Theme!</h2>
         </div>
         <div className="container">
--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './Hero.module.scss';
+
 export const Hero = () => {
   return (
-    <section id="hero" className="hero is-dark is-bold is-small project-title">
+    <section className={`hero is-dark is-bold is-small ${styles.container}`}>
       <div className="columns hero-body mt-6">
         <div className="column is-one-fifth has-text-centered">
           <i className="is-white fas fa-gamepad fa-5x"></i>
@@ -10,7 +12,7 @@ export const Hero = () => {
         <div className="hero-container column container has-text-left">
           <h1 className="title app-title">AniGame</h1>
           <br />
-          <h2 className="subtitle app-description">
+          <h2 className={`subtitle ${styles.description}`}>
             Search for your favorite video games to find related Anime!
           </h2>
         </div>
